2f7e1b4824f5eb3db70332db40f745e2fab39169
Mustafa Enes Güneruz <mustafaguneruz19@gmail.com>
2024-07-09 18:58:30 +0300

    Clear some files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello Next.js!
-    </main>
-  );
+  return <div>Hello Next.js!</div>;
 }
